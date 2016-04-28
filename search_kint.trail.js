1c6a671852ee26542f305058eb9fc2191498c7f9
Felix Eve <felix@monkii.com>
2016-04-28 15:53:05 +1000

    When getting the path to a field return the getter. E.g. $account->get('field_first_name')->value

--- search_kint.trail.js
+++ search_kint.trail.js
@@ -19,26 +19,34 @@
         $(this).append('<span class="kint-get-path"><a href="#">' + Drupal.t('Get path') + '</a></span>');
       });
 
+      var field_name;
+
       $('.kint-get-path a').once().click(function (e) {
         e.preventDefault();
 
         var pathItems = [];
         buildPath($(this).parents('dl').first(), pathItems);
-        pathItems.reverse();
 
-        var path = '$var';
-        $.each(pathItems, function (index, item) {
-          if (item.label === '$args') {
-            return true;
-          }
-          var prevItem = pathItems[index - 1];
-          if (prevItem.type === 'array') {
-            path += '[' + item.label + ']';
-          }
-          else if (prevItem.type.substring(0, 7) === 'object ') {
-            path += '->' + item.label;
-          }
-        });
+        if(typeof field_name == 'undefined') {
+
+          pathItems.reverse();
+          var path = '$var';
+          $.each(pathItems, function (index, item) {
+            if (item.label === '$args') {
+              return true;
+            }
+            var prevItem = pathItems[index - 1];
+            if (prevItem.type === 'array') {
+              path += '[' + item.label + ']';
+            }
+            else if (prevItem.type.substring(0, 7) === 'object ') {
+              path += '->' + item.label;
+            }
+          });
+
+        } else {
+          path = '$var->get(\'' + field_name + '\')->value';
+        }
 
         $(this).addClass('hidden');
         $(this).after('<input id="kint-path-value" type="text" value="' + path + '" />');
@@ -54,6 +62,13 @@
           return;
         }
         var label = $current.find('> dt dfn').text().trim();
+
+        if($current.parents('.kint-report').length) {
+          // We are in a kint report so return the getter for this field
+          if($current.parents().eq(3).hasClass('kint-report')) {
+            field_name = $current.parent().prev().text();
+          }
+        }
 
         // If uppermost dl set label to $args so it gets included.
         if (!$current.parents('dl').length) {
